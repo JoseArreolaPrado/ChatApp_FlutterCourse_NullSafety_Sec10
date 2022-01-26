b79cda83f0ad0a12583f5a0f5665d7f69838c362
Jose Arreola <joseearreola@gmail.com>
2022-01-26 08:43:30 -0600

    Chat App Migrated Null Safety

---
+++ lib/services/usuarios_service.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:http/http.dart' as http;
+
+import 'package:chat/models/usuario.dart';
+import 'package:chat/models/usuarios_response.dart';
+import 'package:chat/services/auth_service.dart';
+
+import 'package:chat/global/environment.dart';
+
+class UsuariosService {
+  Future<List<Usuario>?> getUsuarios() async {
+    try {
+      final uri = Uri.parse('${Environment.apiUrl}/usuarios');
+      final resp = await http.get(uri, headers: {
+        'Content-Type': 'application/json',
+        'x-token': await (AuthService.getToken() as FutureOr<String>)
+      });
+
+      final usuariosResponse = usuariosResponseFromJson(resp.body);
+
+      return usuariosResponse.usuarios;
+    } catch (e) {
+      return [];
+    }
+  }
+}
